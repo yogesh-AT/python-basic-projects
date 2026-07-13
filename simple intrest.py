6067a039b97d88ffb47248cf12710b805e1f2a66
yogesh-AT <atyogesh2009@gmail.com>
2026-07-13 17:10:54 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,12 @@
+print('principal amount = ')
+p = int(input())
+
+print('time period /(in years/) ' )
+t = float(input())
+
+print('rate of intrest = ')
+r = float(input())
+s =  (p*t*r) / 100
+
+print('the simple intrest is ')
+print(s)
