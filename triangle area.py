cf5067510d253c46a0d90dcace348287c742e4d5
yogesh-AT <atyogesh2009@gmail.com>
2026-07-13 17:03:02 +0530

    Add files via upload

---
+++ triangle area.py
@@ -0,0 +1,9 @@
+print ('how many sides ') #to confirm it is a triangle
+n = int(input())
+if n==3:
+     print('the length of base and height of triange are ')
+base = int(input())
+height = int(input())
+a = ( base * height )/2
+print ('the area of triangle is',a)
+
