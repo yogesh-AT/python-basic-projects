9afd64b110c943bd5ad21fd87a7ffed0cde25002
Yogesh <atyogesh2009@gmail.com>
2026-07-15 22:30:57 +0530

    thanking the users at the end

--- leapyear.py
+++ leapyear.py
@@ -4,3 +4,7 @@ if year%4==0 and year%100 !=0  or year%400==0:
     print('it is a leap year')
 else :
     print('it is not a leap year')
+
+print('thank you for using this program')   
+
+#end of the code
